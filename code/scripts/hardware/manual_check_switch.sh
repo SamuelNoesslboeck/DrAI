bc222208b219d2b07956f6c5f3b04900c5811816
Samuel Nösslböck <samuel.noesslboeck@gmail.com>
2024-03-17 20:08:08 +0100

    fixed script

--- code/scripts/hardware/manual_check_switch.sh
+++ code/scripts/hardware/manual_check_switch.sh
@@ -4,14 +4,15 @@ echo
 echo Manually check if a endswitch is working
 echo  -> ATTENTION: Must be executed in the projects root folder
 echo
-echo Selected switch: $3
-echo  -> Pin: $2
+echo Selected switch: $0
+echo  -> Pin: $1
 echo
 
 sh "code/scripts/env.sh"
+echo
 
 cd "code/syact"
 
-cargo run --features=rasp --example hardware-meas-endswitch $2
+cargo run --features=rasp --example hardware-meas-endswitch $0
 
 cd "../.."
