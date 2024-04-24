ef448215cb3e502a381f67980c99dd8862bf350a
Samuel Nösslböck <samuel.noesslboeck@gmail.com>
2024-04-24 12:52:10 +0200

    sync

--- code/drake_ctrl/bin/drake_table_test.rs
+++ code/drake_ctrl/bin/drake_table_test.rs
@@ -1,7 +1,17 @@
-use clap::{command, arg, value_parser};
+use std::io::{stdout, stdin, Read, Write};
 
+use clap::{command, arg, value_parser};
 use drake::servo_table::ServoTable;
 use syact::Setup;
+
+// Process
+    fn pause() {
+        let mut stdout = stdout();
+        stdout.write(b"Press Enter to continue...").unwrap();
+        stdout.flush().unwrap();
+        stdin().read(&mut [0]).unwrap();
+    }
+// 
 
 fn main() {
         // Init logging
@@ -28,12 +38,17 @@ fn main() {
         
         if state == "open" {
             table.set_all_open().unwrap();
+            println!("Servos are now open!")
         } else if state == "closed" {
             table.set_all_closed().unwrap();
+            println!("Servos are now closed!");
         } else if state == "standby" {
+            println!("Servos are now on standby!");
             table.set_all_standby().unwrap();
         } else {
             println!("Invalid state given!");
         }
     }
+
+    pause();
 }
